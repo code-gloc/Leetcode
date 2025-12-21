4e1057b621a92c2aba4ed0a0f59526ba97843e79
Ashish Delmiya <ashishdelmiya02@gmail.com>
2025-12-21 21:49:36 +0530

    backend connect to frontend

--- FRONTEND/src/utils/axiosclient.js
+++ FRONTEND/src/utils/axiosclient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient=axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://codex-rzgk.onrender.com",
     withCredentials:true,
     headers:{
         'Content-Type': 'application/json'
