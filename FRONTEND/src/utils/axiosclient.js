a63a635f37e60546bd88e0f20f289acf78bde8c0
Ashish Delmiya <ashishdelmiya02@gmail.com>
2026-04-28 17:03:23 +0530

    setup env and connect frontend to deployed backend

--- FRONTEND/src/utils/axiosclient.js
+++ FRONTEND/src/utils/axiosclient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient=axios.create({
-    baseURL:"https://codex-rzgk.onrender.com",
+    baseURL:"http://localhost:3000",
     withCredentials:true,
     headers:{
         'Content-Type': 'application/json'
